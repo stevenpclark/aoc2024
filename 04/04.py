f8ec9a051b4b6a8670c34b51ca39379d135a8365
Steven Clark <sclark@exptechinc.com>
2024-12-04 15:03:51 -0500

    day 4 star 2

--- 04/04.py
+++ 04/04.py
@@ -18,15 +18,28 @@ def solve(fn, part2=False):
 
     num_matches = 0
     for r, c in np.ndindex(nr, nc):
-        ch = m[r,c]
-        for dr, dc in dirs:
-            found = True
-            for i, x in enumerate(target):
-                if m[r+i*dr,c+i*dc] != x:
-                    found = False
-                    break
-            if found:
-                num_matches += 1
+        if not part2:
+            for dr, dc in dirs:
+                found = True
+                for i, x in enumerate(target):
+                    if m[r+i*dr,c+i*dc] != x:
+                        found = False
+                        break
+                if found:
+                    num_matches += 1
+        else:
+            if m[r,c] == 'A':
+                num_here = 0
+                if m[r-1,c-1] == 'M' and m[r+1][c+1] == 'S':
+                    num_here += 1
+                elif m[r+1,c+1] == 'M' and m[r-1][c-1] == 'S':
+                    num_here += 1
+                if m[r-1,c+1] == 'M' and m[r+1][c-1] == 'S':
+                    num_here += 1
+                elif m[r+1,c-1] == 'M' and m[r-1][c+1] == 'S':
+                    num_here += 1
+                if num_here == 2:
+                    num_matches += 1
 
     return num_matches
 
@@ -35,6 +48,7 @@ def main():
     assert solve('test.txt') == 18
     print(solve('input.txt'))
     assert solve('test.txt', part2=True) == 9
+    print(solve('input.txt', part2=True))
 
 if __name__ == '__main__':
     main()
