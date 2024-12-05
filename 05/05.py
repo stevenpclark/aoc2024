48ee65ecd4db2ac9ec562d594ca2d10a95205f64
Steven Clark <sclark@exptechinc.com>
2024-12-05 14:42:37 -0500

    day 5 part 2

--- 05/05.py
+++ 05/05.py
@@ -1,3 +1,20 @@
+def sort_and_score(update, rules):
+    #there's almost certainly a smarter way of doing this, but eh, whatever
+    valid = False
+    while not valid:
+        valid = True
+        for a, b in rules:
+            try:
+                a_ind, b_ind = update.index(a), update.index(b)
+            except ValueError:
+                continue
+            if a_ind >= b_ind:
+                #move b to just after a
+                update.insert(a_ind+1, update.pop(b_ind))
+                valid = False
+    return update[len(update)//2]
+
+
 def solve(fn, part2=False):
     with open(fn, 'r') as f:
         lines = f.read().splitlines()
@@ -15,18 +32,20 @@ def solve(fn, part2=False):
             updates.append([int(s) for s in li.split(',')])
 
     total = 0
-    for up in updates:
+    for update in updates:
         update_valid = True
         for a, b in rules:
             try:
-                a_ind, b_ind = up.index(a), up.index(b)
+                a_ind, b_ind = update.index(a), update.index(b)
             except ValueError:
                 continue
             if a_ind >= b_ind:
                 update_valid = False
                 break
-        if update_valid:
-            total += up[len(up)//2]
+        if update_valid and not part2:
+            total += update[len(update)//2]
+        if not update_valid and part2:
+            total += sort_and_score(update, rules)
 
     return total
 
@@ -34,6 +53,8 @@ def solve(fn, part2=False):
 def main():
     assert solve('test.txt') == 143
     print(solve('input.txt'))
+    assert solve('test.txt', part2=True) == 123
+    print(solve('input.txt', part2=True))
 
 if __name__ == '__main__':
     main()
